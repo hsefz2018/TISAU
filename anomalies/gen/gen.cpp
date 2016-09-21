be9e75b1ffc892b8cb371ced2bbc179915390080
Pisces000221 <1786762946@qq.com>
2016-09-21 19:28:32 +0800

    Rev

--- anomalies/gen/gen.cpp
+++ anomalies/gen/gen.cpp
@@ -7,13 +7,14 @@ static const int MAXN = 150005 * 3;
 
 int n, max_g;
 int g[MAXN * 3];
+int mapped[MAXN * 6];
 
 inline void rand_distinct_2(int &a, int &b) {
-    do { a = rand() % max_g; b = rand() % max_g; } while (a == b);
+    do { a = rand() % (n * 3); b = rand() % (n * 3); } while (a == b);
 }
 inline void rand_distinct_3(int &a, int &b, int &c) {
     rand_distinct_2(a, c);
-    do b = rand() % max_g; while (b == a || b == c);
+    do b = rand() % (n * 3); while (b == a || b == c);
 }
 
 int main(int argc, char *argv[])
@@ -32,8 +33,26 @@ int main(int argc, char *argv[])
         for (int i = 1; i < n * 3; ++i) g[i] = i;
         std::random_shuffle(g + 1, g + n * 3);
     } else {
+        if (max_g > n * 3) {
+            do {
+                mapped[0] = 0;
+                for (int i = 1; i < n * 6; ++i) mapped[i] = rand() % max_g;
+                std::sort(mapped + 1, mapped + n * 6);
+                if (std::unique(mapped + 1, mapped + n * 6) - &mapped[0] >= n * 3) {
+                    std::random_shuffle(mapped + 1, mapped + n * 6);
+                    break;
+                }
+            } while (1);
+        } else for (int i = 0; i < max_g; ++i) mapped[i] = i;
         do rand_distinct_2(g[1], g[2]); while (g[1] == 0 || g[2] == 0);
         for (int i = 1; i < n; ++i) rand_distinct_3(g[i * 3], g[i * 3 + 1], g[i * 3 + 2]);
+        for (int i = 0; i < n * 3; ++i) g[i] = mapped[g[i]];
+        for (int i = 0; i < n / 100; ++i) {
+            int row = rand() % (n - 1) + 1;
+            do
+                g[row * 3 + rand() % 3] = rand() % max_g;
+            while (g[row * 3] == g[row * 3 + 1] || g[row * 3] == g[row * 3 + 2] || g[row * 3 + 1] == g[row * 3 + 2]);
+        }
     }
 
     printf("%d\n", n);
